9e09a0b990f095eb518cc504b53621a571826965
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-09-09 14:43:02 +0100

    LPD-104976 Give every layout preview its own output data

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/renderer/LayoutPreviewRendererImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/renderer/LayoutPreviewRendererImpl.java
@@ -71,6 +71,8 @@ public class LayoutPreviewRendererImpl implements LayoutPreviewRenderer {
 		LayoutStructure originalLayoutStructure =
 			(LayoutStructure)httpServletRequest.getAttribute(
 				LayoutWebKeys.LAYOUT_STRUCTURE);
+		Object originalOutputData = httpServletRequest.getAttribute(
+			WebKeys.OUTPUT_DATA);
 		boolean originalPortletDecorate = GetterUtil.getBoolean(
 			httpServletRequest.getAttribute(WebKeys.PORTLET_DECORATE));
 		long[] originalSegmentsExperienceIds = GetterUtil.getLongValues(
@@ -93,6 +95,7 @@ public class LayoutPreviewRendererImpl implements LayoutPreviewRenderer {
 			httpServletRequest.setAttribute(
 				SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS,
 				new long[] {segmentsExperienceId});
+			httpServletRequest.setAttribute(WebKeys.OUTPUT_DATA, null);
 			httpServletRequest.setAttribute(
 				WebKeys.PORTLET_DECORATE, Boolean.FALSE);
 
@@ -166,6 +169,8 @@ public class LayoutPreviewRendererImpl implements LayoutPreviewRenderer {
 				SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS,
 				originalSegmentsExperienceIds);
 			httpServletRequest.setAttribute(WebKeys.LAYOUT, originalLayout);
+			httpServletRequest.setAttribute(
+				WebKeys.OUTPUT_DATA, originalOutputData);
 			httpServletRequest.setAttribute(
 				WebKeys.PORTLET_DECORATE, originalPortletDecorate);
 			httpServletRequest.setAttribute(
